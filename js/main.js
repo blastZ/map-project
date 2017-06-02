278f5359a0b19cffa06cb32925830eccd732e5ac
blastz <touchzh1996@gmail.com>
2017-06-02 22:43:39 +0800

    Add nav-panel

--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
+var map;
+var markers = [];
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center: {lng: 120.350, lat: 30.310} //latitude 纬度 longitude 经度
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lng: -73.9980244, lat: 40.7413549} //latitude 纬度 longitude 经度
     });
     var locations = [
-        {position: {lng: 122.330, lat: 32.322}, title: 'location-1'},
-        {position: {lng: 123.330, lat: 33.322}, title: 'location-2'},
-        {position: {lng: 121.330, lat: 34.322}, title: 'location-3'},
-        {position: {lng: 120.330, lat: 31.322}, title: 'location-4'},
-        {position: {lng: 124.330, lat: 30.322}, title: 'location-5'},
-        {position: {lng: 125.330, lat: 29.322}, title: 'location-6'},
-        {position: {lng: 126.330, lat: 35.322}, title: 'location-7'}
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
     ];
-    var markers = [];
-    var largeInfoWindow = google.maps.InfoWindow();
-    var bounds = google.maps.LatLngBounds();
+    var largeInfoWindow = new google.maps.InfoWindow();
     for(var i=0; i<locations.length; i++) {
-        var position = locations[i].position;
+        var position = locations[i].location;
         var title = locations[i].title;
         var marker = new google.maps.Marker({
-            map: map,
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
             id: i
         });
         markers.push(marker);
-        google.maps.event.addListener(marker, 'click', populateInfoWindow(marker, largeInfoWindow));
-        bounds.extend(marker[i].position);
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfoWindow);
+        });
     }
-    map.fitBounds(bounds);
 }
 
 function populateInfoWindow(marker, infoWindow) {
@@ -37,8 +35,39 @@ function populateInfoWindow(marker, infoWindow) {
         infoWindow.marker = marker;
         infoWindow.setContent('<div>' + marker.title + '</div>');
         infoWindow.open(map, marker);
-        google.maps.event.addListener(infoWindow, 'closeclick', function(){
+        infoWindow.addListener('closeclick', function() {
             infoWindow.setMarker = null;
-        });
+        })
     }
 }
+
+function showListings() {
+    var bounds = new google.maps.LatLngBounds();
+    for(var i=0; i<markers.length; i++) {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+}
+
+function hideListings() {
+    for(var i=0; i<markers.length; i++) {
+        markers[i].setMap(null);
+    }
+}
+
+$('#nav-button').click(function() {
+    $('#nav-panel').show(500);
+});
+
+$('#nav-panel li').first().click(function() {
+    $('#nav-panel').hide(500);
+});
+
+$('#nav-panel li:nth-child(2)').click(function() {
+    showListings();
+});
+
+$('#nav-panel li:nth-child(3)').click(function() {
+    hideListings();
+})
