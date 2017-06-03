825847623a28d42d8ce77f98412adb687f979ece
blastz <touchzh1996@gmail.com>
2017-06-03 22:20:16 +0800

    Add night-mode style and street view

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var map;
 var markers = [];
+var nightModeStyleType;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-        center: {lng: -73.9980244, lat: 40.7413549} //latitude 纬度 longitude 经度
+        center: {lng: -73.9980244, lat: 40.7413549}, //latitude 纬度 longitude 经度
+        mapTypeControl: false
     });
     var locations = [
         {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
@@ -13,6 +16,19 @@ function initMap() {
         {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
         {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
     ];
+    var defaultMarker = {
+        url: 'imgs/marker-default.png',
+        scaledSize: new google.maps.Size(32, 32),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(16, 32)
+    };
+    var hoverMarker = {
+        url: 'imgs/marker-hover.png',
+        scaledSize: new google.maps.Size(32, 32),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(16, 32)
+    };
+
     var largeInfoWindow = new google.maps.InfoWindow();
     for(var i=0; i<locations.length; i++) {
         var position = locations[i].location;
@@ -21,24 +37,129 @@ function initMap() {
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
-            id: i
+            id: i,
+            icon: defaultMarker,
+            draggable: true
         });
         markers.push(marker);
-        marker.addListener('click', function() {
+        marker.addListener('mouseup', function() {
             populateInfoWindow(this, largeInfoWindow);
         });
+        marker.addListener('mouseover', function() {
+            this.setIcon(hoverMarker);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultMarker);
+        });
     }
+
+    nightModeStyleType = new google.maps.StyledMapType([
+                {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+                {
+                  featureType: 'administrative.locality',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+                },
+                {
+                  featureType: 'poi',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+                },
+                {
+                  featureType: 'poi.park',
+                  elementType: 'geometry',
+                  stylers: [{color: '#263c3f'}]
+                },
+                {
+                  featureType: 'poi.park',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#6b9a76'}]
+                },
+                {
+                  featureType: 'road',
+                  elementType: 'geometry',
+                  stylers: [{color: '#38414e'}]
+                },
+                {
+                  featureType: 'road',
+                  elementType: 'geometry.stroke',
+                  stylers: [{color: '#212a37'}]
+                },
+                {
+                  featureType: 'road',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#9ca5b3'}]
+                },
+                {
+                  featureType: 'road.highway',
+                  elementType: 'geometry',
+                  stylers: [{color: '#746855'}]
+                },
+                {
+                  featureType: 'road.highway',
+                  elementType: 'geometry.stroke',
+                  stylers: [{color: '#1f2835'}]
+                },
+                {
+                  featureType: 'road.highway',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#f3d19c'}]
+                },
+                {
+                  featureType: 'transit',
+                  elementType: 'geometry',
+                  stylers: [{color: '#2f3948'}]
+                },
+                {
+                  featureType: 'transit.station',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+                },
+                {
+                  featureType: 'water',
+                  elementType: 'geometry',
+                  stylers: [{color: '#17263c'}]
+                },
+                {
+                  featureType: 'water',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#515c6d'}]
+                },
+                {
+                  featureType: 'water',
+                  elementType: 'labels.text.stroke',
+                  stylers: [{color: '#17263c'}]
+                }
+    ]);
 }
 
 function populateInfoWindow(marker, infoWindow) {
-    if(infoWindow.marker !== marker) {
-        infoWindow.marker = marker;
-        infoWindow.setContent('<div>' + marker.title + '</div>');
-        infoWindow.open(map, marker);
-        infoWindow.addListener('closeclick', function() {
-            infoWindow.setMarker = null;
-        })
+    infoWindow.setContent('');
+    infoWindow.marker = marker;
+    var streetViewService = new google.maps.StreetViewService();
+    var radius = 50;
+    function getStreetView(data, status) {
+        if(status === google.maps.StreetViewStatus.OK) {
+            var nearStreetViewLocation = data.location.latLng;
+            var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
+            infoWindow.setContent('<div>' + data.location.shortDescription + '</div><div id="pano"></div>');
+            var panoramaOptions = {
+                position: nearStreetViewLocation,
+                pov: {
+                    // heading 左右方向，pitch 上下角度
+                    heading: heading,
+                    pitch: 30
+                }
+            };
+            var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+        }else {
+            infoWindow.setContent('<div>' + data.location.shortDescription + '</div>' + '<div>No Street View Found</div>');
+        }
     }
+    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    infoWindow.open(map, marker);
 }
 
 function showListings() {
@@ -56,6 +177,17 @@ function hideListings() {
     }
 }
 
+function changeMapStyle(styleMode) {
+    if(styleMode === 'nightMode') {
+        if(map.mapTypeId === 'nightMode') {
+            map.setMapTypeId('roadmap');
+        }else {
+            map.mapTypes.set('nightMode', nightModeStyleType);
+            map.setMapTypeId('nightMode');
+        }
+    }
+}
+
 $('#nav-button').click(function() {
     $('#nav-panel').show(500);
 });
@@ -70,4 +202,8 @@ $('#nav-panel li:nth-child(2)').click(function() {
 
 $('#nav-panel li:nth-child(3)').click(function() {
     hideListings();
-})
+});
+
+$('#nav-panel li:nth-child(4)').click(function() {
+    changeMapStyle('nightMode');
+});
